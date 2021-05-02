0a9f800594b7cee9f462815485b0d5cd7e856fb1
Nicolas Lema <n.lema@uniandes.edu.co>
2021-05-01 23:34:00 -0500

    :rocket: Deploy Stuff

--- despieces/settings/prod.py
+++ despieces/settings/prod.py
@@ -1,3 +1,15 @@
 from .base import *
-import django_heroku
-django_heroku.settings(locals())
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT")
+    }
+}
