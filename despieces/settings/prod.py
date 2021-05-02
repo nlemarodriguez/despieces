d5167179fce27ff5e9621fc42965fa145cb7917c
Nicolas Lema <n.lema@uniandes.edu.co>
2021-05-02 13:18:16 -0500

    :rocket: Deploy Stuff

--- despieces/settings/prod.py
+++ despieces/settings/prod.py
@@ -1,19 +1,21 @@
-from .base import *
-
 import environ
+from .base import *
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
 
+DEBUG = env('DEBUG')
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {'default': env.db_url_config(engine='django.db.backends.postgresql_psycopg2'), }
+    # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
+    'default': env.db(),
 }
 
-DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
