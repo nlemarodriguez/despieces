422980db725284a3f30081d7061f5717c68ccb98
Nicolas Lema <n.lema@uniandes.edu.co>
2021-05-01 21:52:45 -0500

    :rocket: Deploy Stuff

--- despieces/settings/local.py
+++ despieces/settings/local.py
@@ -2,17 +2,3 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(), override=True)
 
 from .base import *
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT")
-    }
-}
--- despieces/settings/prod.py
+++ despieces/settings/prod.py
@@ -2,9 +2,3 @@ import django_heroku
 from .base import *
 django_heroku.settings(locals())
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'URL': os.getenv("DATABASE_URL"),
-    }
-}
