9bd6c73cc00fc5cdc0446774102d41119db2f7f9
Nicolas Lema <n.lema@uniandes.edu.co>
2021-05-02 01:11:20 -0500

    :rocket: Deploy Stuff

--- despieces/settings/prod.py
+++ despieces/settings/prod.py
@@ -1,3 +1,12 @@
 from .base import *
 
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    }
+}
+
 ALLOWED_HOSTS = ['despieces-dev.herokuapp.com']
