4493aece714e1d76fc9dc0058f6a0670053c6245
Nicolas Lema <n.lema@uniandes.edu.co>
2021-05-02 00:41:55 -0500

    :rocket: Deploy Stuff

--- despieces/settings/prod.py
+++ despieces/settings/prod.py
@@ -6,6 +6,11 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
