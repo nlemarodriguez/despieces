961185a34b6d658178d5c68255dca257072910da
Nicolas Lema <n.lema@uniandes.edu.co>
2021-05-02 12:49:03 -0500

    :rocket: Deploy Stuff

--- despieces/settings/local.py
+++ despieces/settings/local.py
@@ -1,18 +1,14 @@
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv(), override=True)
-
 from .base import *
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# reading .env file
+environ.Env.read_env()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT")
-    }
+    'default': {'default': env.db(), }
 }
--- despieces/settings/prod.py
+++ despieces/settings/prod.py
@@ -1,26 +1,12 @@
 from .base import *
-import environ
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT")
-    }
+    'default': {'default': env.db(), }
 }
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-
-# False if not in os.environ
 DEBUG = env('DEBUG')
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
