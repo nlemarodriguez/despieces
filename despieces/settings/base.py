5afc05a1b1de00a2bb631641cedac24cb585668f
Nicolas Lema <n.lema@uniandes.edu.co>
2021-05-01 22:06:17 -0500

    :rocket: Deploy Stuff

--- despieces/settings/base.py
+++ despieces/settings/base.py
@@ -75,20 +75,6 @@ WSGI_APPLICATION = 'despieces.wsgi.application'
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT")
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
