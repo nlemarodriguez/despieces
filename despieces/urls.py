25d6dfc9171222409170546c64b6e07f6805a5d1
Nicolas Lema <n.lema@uniandes.edu.co>
2021-05-22 00:34:11 -0500

    :sparkles: Products app

--- despieces/urls.py
+++ despieces/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.home.urls')),
-    path('', include('apps.quotations.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include('apps.quotations.urls')),
+    path('', include('apps.products.urls'))
+]
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
