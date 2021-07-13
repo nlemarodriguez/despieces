49adcdf5b6bc42c2887161603d5a056ce1c03233
Nicolas Lema <n.lema@uniandes.edu.co>
2021-07-13 00:50:47 -0500

    :pencil2: Fix typo in forms.py

---
+++ apps/products/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django_measurement.forms import MeasurementField
+from measurement.measures import Distance
+
+from .models import Material
+
+
+class MaterialForm(forms.ModelForm):
+    class Meta:
+        model = Material
+        fields = ['name', 'description', 'price', 'is_measurable', 'width', 'high', 'photo']
+
+        width = MeasurementField(Distance)
+        high = MeasurementField(Distance)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['width'].widget.attrs.update({'class': 'form-control', 'min': '0', 'placeholder': 'Ancho'})
+        self.fields['high'].widget.attrs.update({'class': 'form-control', 'min': '0', 'placeholder': 'Alto'})
+
+        self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Nombre'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Descripción',
+                                                        'rows': '3'})
+        self.fields['price'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Precio', 'min': '0'})
+        self.fields['is_measurable'].widget.attrs.update({'class': 'form-check-input', 'type': 'checkbox'})
