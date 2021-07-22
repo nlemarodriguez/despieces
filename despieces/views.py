c99628303d94a9ae32fbfb2544c3cc9ac4764186
Nicolas Lema <n.lema@uniandes.edu.co>
2021-07-21 21:52:56 -0500

    :passport_control: Add control access only for companies and authenticated users

--- despieces/views.py
+++ despieces/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 
 def custom_page_not_found_view(request, exception):
-    return render(request, 'home/errors/404.html', status=404)
+    return render(request, 'errors/404.html', status=404)
 
 
 def custom_error_view(request, exception=None):
-    return render(request, "home/errors/500.html", status=500)
+    return render(request, "errors/500.html", status=500)
+
+
+def custom_permission_view(request, exception=None):
+    return render(request, "errors/403.html", status=403)
 
