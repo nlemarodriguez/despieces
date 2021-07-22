bbf6f10d4825ebf419a6130ea6c409b4c6f7e332
Nicolas Lema <n.lema@uniandes.edu.co>
2021-07-22 01:22:42 -0500

    :sparkles: Login and Logout

---
+++ apps/registration/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
+from .forms import UserLoginForm
+
+app_name = 'registration_app'
+
+# Add Django site authentication urls (for login, logout, password management)
+urlpatterns = [
+    path('cuenta/login/', auth_views.LoginView.as_view(template_name="registration/login.html",
+                                                       authentication_form=UserLoginForm), name='login'),
+    path('cuenta/', include('django.contrib.auth.urls')),
+
+]
