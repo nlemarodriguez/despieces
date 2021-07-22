16909baa2a2fce93e757a33c7c139a88611983ef
Nicolas Lema <n.lema@uniandes.edu.co>
2021-07-21 23:19:23 -0500

    :art: Add / at the end of all urls

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 app_name = 'products_app'
 
 urlpatterns = [
-    path('productos', views.ProductsList.as_view(), name='products_list'),
-    path('producto', views.ProductCreate.as_view(), name='product_create'),
-    path('materiales', views.MaterialsList.as_view(), name='materials_list'),
-    path('material', views.MaterialCreate.as_view(), name='material_create'),
+    path('productos/', views.ProductsList.as_view(), name='products_list'),
+    path('producto/', views.ProductCreate.as_view(), name='product_create'),
+    path('materiales/', views.MaterialsList.as_view(), name='materials_list'),
+    path('material/', views.MaterialCreate.as_view(), name='material_create'),
     path('material/<pk>/', views.MaterialUpdate.as_view(), name='material_update'),
     path('material/delete/<pk>/', views.MaterialDelete.as_view(), name='material_delete'),
 ]
--- apps/quotations/urls.py
+++ apps/quotations/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = 'quotations_app'
 
 urlpatterns = [
-    path('cotizaciones', views.QuotationsList.as_view(), name='quotations_list'),
-    path('cotizaciones/<int:pk>', views.QuotationsDetail.as_view(), name='quotations_detail'),
-    path('cotizacion', views.QuotationCreate.as_view(), name='quotations_create'),
+    path('cotizaciones/', views.QuotationsList.as_view(), name='quotations_list'),
+    path('cotizaciones/<int:pk>/', views.QuotationsDetail.as_view(), name='quotations_detail'),
+    path('cotizacion/', views.QuotationCreate.as_view(), name='quotations_create'),
 ]
