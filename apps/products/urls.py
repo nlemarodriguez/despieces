8cbd4700f400c6dd15ecd8727686a183dc8e1840
Nicolas Lema <n.lema@uniandes.edu.co>
2021-07-23 00:29:22 -0500

    :truck: Change urls names

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -5,9 +5,9 @@ app_name = 'products_app'
 
 urlpatterns = [
     path('productos/', views.ProductsList.as_view(), name='products_list'),
-    path('producto/', views.ProductCreate.as_view(), name='product_create'),
+    path('productos/nuevo/', views.ProductCreate.as_view(), name='product_create'),
     path('materiales/', views.MaterialsList.as_view(), name='materials_list'),
-    path('material/', views.MaterialCreate.as_view(), name='material_create'),
-    path('material/<pk>/', views.MaterialUpdate.as_view(), name='material_update'),
-    path('material/delete/<pk>/', views.MaterialDelete.as_view(), name='material_delete'),
+    path('materiales/nuevo/', views.MaterialCreate.as_view(), name='material_create'),
+    path('materiales/<pk>/editar/', views.MaterialUpdate.as_view(), name='material_update'),
+    path('materiales/<pk>/borrar/', views.MaterialDelete.as_view(), name='material_delete'),
 ]
