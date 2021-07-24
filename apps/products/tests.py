2c3ab024c7bae9b6e3e3e3d617df58714be6bd02
Nicolas Lema <n.lema@uniandes.edu.co>
2021-07-24 18:47:19 -0500

    :white_check_mark: Add test to products apps

--- apps/products/tests.py
+++ apps/products/tests.py
@@ -1,7 +1,10 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser
+from django.test import TestCase, TransactionTestCase
 from django.urls import reverse
+from django.test.client import Client
 
 from .models import Product, Material, Composition, Rule
+from ..users.models import User
 
 
 class ProductListTest(TestCase):
@@ -26,3 +29,46 @@ class ProductListTest(TestCase):
                             composition=composition)
         response = self.client.get(reverse('products_app:products_list'))
         self.assertEquals(response.context['products_list'][0].rules, 2)
+
+
+class MaterialListTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        User.objects.create_user('company_without_materials@a.com', 'Admin00', is_company=True)
+        company_with_materials = User.objects.create_user('company_with_materials@a.com', 'Admin00', is_company=True)
+        User.objects.create_user('dependent_user@a.com', 'Admin00', company=company_with_materials)
+        User.objects.create_user('independent_user@a.com', 'Admin00')
+        Material.objects.create(name='material1', description='material1', price=1, user_owner=company_with_materials)
+        Material.objects.create(name='material2', description='material2', price=1)
+
+    def test_company_permissions(self):
+        # Not logged user is redirect to login page
+        response = self.client.get(reverse(f'products_app:materials_list'))
+        self.assertRedirects(response, '/cuenta/login/?next=/materiales/')
+
+        # Only companies can access
+        self.client.login(username='dependent_user@a.com', password='Admin00')
+        response = self.client.get(reverse(f'products_app:materials_list'))
+        self.assertEqual(response.status_code, 403)
+
+        # Company can access
+        self.client.login(username='company_without_materials@a.com', password='Admin00')
+        response = self.client.get(reverse(f'products_app:materials_list'))
+        self.assertEqual(response.status_code, 200)
+
+        # Independent user can access
+        self.client.login(username='independent_user@a.com', password='Admin00')
+        response = self.client.get(reverse(f'products_app:materials_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_company_ownership_material(self):
+        # Company without materials get empty list
+        self.client.login(username='company_without_materials@a.com', password='Admin00')
+        response = self.client.get(reverse(f'products_app:materials_list'))
+        self.assertQuerysetEqual(response.context['materials_list'], [])
+
+        # Company with materials get only his materials
+        self.client.login(username='company_with_materials@a.com', password='Admin00')
+        response = self.client.get(reverse(f'products_app:materials_list'))
+        self.assertEquals(response.context['materials_list'][0].name, 'material1')
